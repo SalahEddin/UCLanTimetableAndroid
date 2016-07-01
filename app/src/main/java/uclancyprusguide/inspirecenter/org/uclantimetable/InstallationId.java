b7f7712e457de68c37b376e3e7889adac6b26301
salah eddin alshaal <salshaal@gmail.com>
2016-07-01 11:02:26 +0300

    added icons and text for the timetable functionality

---
+++ app/src/main/java/uclancyprusguide/inspirecenter/org/uclantimetable/InstallationId.java
@@ -0,0 +1,7 @@
+package uclancyprusguide.inspirecenter.org.uclantimetable;
+/**
+ * @author Nearchos
+ *         Created: 25-Mar-16
+ */
+public class InstallationId {
+}
---
+++ app/src/main/java/uclancyprusguide/inspirecenter/org/uclantimetable/util/SharingTools.java
@@ -0,0 +1,33 @@
+package uclancyprusguide.inspirecenter.org.uclantimetable.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import uclancyprusguide.inspirecenter.org.uclantimetable.R;
+
+/**
+ * @author Nearchos Paspallis
+ * 03/01/2015 / 20:02.
+ */
+public class SharingTools
+{
+    static public void shareApp(final Context context)
+    {
+        final String title = context.getString(R.string.app_name);
+        final String text = context.getString(R.string.Install);
+        final Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, title);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, text);
+        final String shareText = context.getString(R.string.Share);
+        if(IntentUtils.isIntentAvailable(context, shareIntent))
+        {
+            context.startActivity(Intent.createChooser(shareIntent, shareText));
+        }
+        else
+        {
+            Toast.makeText(context, "No apps available for sharing", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
